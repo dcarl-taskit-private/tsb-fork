ba23f5b2fccc553f7f302d30c0f8be2ece43ae70
traulfs <traulfs@taskit.de>
2022-11-16 17:58:09 +0100

    16.11.22

--- i2c.go
+++ i2c.go
@@ -2,16 +2,35 @@ package tsb
 
 import (
 	"fmt"
+	"time"
 )
 
 func (s *Server) I2cInit(jack byte) (err error) {
 	CheckJack(jack)
-	//s.jack[jack].ReadChan[TypI2c] = make(chan byte, 1024)
+	s.jack[jack].ReadChan[TypI2c] = make(chan byte, 1024)
 	return nil
 }
 
 func (s *Server) I2cRead(jack byte, adr byte, b []byte) (n int, err error) {
-	return 0, nil
+	var w [3]byte
+	n = len(b)
+	if n > 127 {
+		return 0, fmt.Errorf("only 127 bytes to write are allowed")
+	}
+	w[0] = 128
+	w[1] = adr
+	w[2] = byte(n)
+	td := TsbData{Ch: []byte{byte(jack)}, Typ: []byte{TypI2c}, Payload: w[:3]}
+	s.tdPutCh <- td
+	for i := 0; i < n; i++ {
+		select {
+		case b[i] = <-s.jack[jack].ReadChan[TypI2c]:
+			fmt.Printf("Read from i2c: %x\n", b[i])
+		case <-time.After(1 * time.Second):
+			fmt.Printf("I2cRead Timeout\n")
+		}
+	}
+	return n, nil
 }
 
 func (s *Server) I2cWrite(jack byte, adr byte, b []byte) (n int, err error) {
@@ -21,7 +40,7 @@ func (s *Server) I2cWrite(jack byte, adr byte, b []byte) (n int, err error) {
 	}
 	w[0] = 128
 	w[1] = adr
-	w[2] = byte(len(b))
+	w[2] = byte(len(b) + 128)
 	var i int
 	for i = range b {
 		w[i+3] = b[i]
